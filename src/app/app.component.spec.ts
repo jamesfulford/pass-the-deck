41fd5e5a1557a9a7639ec23b9b74ec806c3deb60
James Fulford <17038886+jamesfulford@users.noreply.github.com>
2024-07-01 20:02:20 -0600

    WIP: /new page improvements

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'take-one-down' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('take-one-down');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, take-one-down');
-  });
 });
--- src/app/new-page/new-page.component.ts
+++ src/app/new-page/new-page.component.ts
@@ -1,46 +1,58 @@
 import { Component } from '@angular/core';
 import type { Option } from '../option.types';
 import { shuffleArray } from '../shuffle';
+import { v4 } from 'uuid';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './new-page.component.html',
   styleUrl: './new-page.component.css',
 })
 export class NewPageComponent {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.route.queryParamMap.subscribe((params) => {
+      const optionsString = params.get('options');
+      const options = optionsString ? optionsString.split(',') : [];
+      this.options = options;
+    });
+  }
+
+  options: string[] = [];
+
+  addOption() {
+    this.options.push('');
+    this.updateQueryParams();
+  }
+  removeOption(i: number) {
+    this.options.splice(i, 1);
+    this.updateQueryParams();
+  }
+  trackByFn(index: number, _item: string): number {
+    return index;
+  }
+  updateQueryParams() {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        options: this.options.join(','),
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
+  setOptions(options: string[]) {
+    this.options = options;
+    this.updateQueryParams();
+  }
+
   public href() {
-    const options: Option[] = [
-      {
-        id: '1',
-        name: 'Angular',
-      },
-      {
-        id: '2',
-        name: 'React',
-      },
-      {
-        id: '3',
-        name: 'Vue',
-      },
-      {
-        id: '4',
-        name: 'Svelte',
-      },
-      {
-        id: '5',
-        name: 'Lit',
-      },
-      {
-        id: '6',
-        name: 'Stencil',
-      },
-      {
-        id: '7',
-        name: 'Polymer',
-      },
-    ];
+    const options: Option[] = this.options.map((s) => ({ id: v4(), name: s }));
     shuffleArray(options);
     return `/pick?options=${btoa(JSON.stringify(options))}`;
   }
